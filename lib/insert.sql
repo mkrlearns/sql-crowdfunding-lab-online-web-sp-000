eafcaaa59764008605a80fa02eda36fd6973a4b0
ryan m <contact@ryanmeek.com>
2020-01-26 21:40:37 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -3,7 +3,7 @@ INSERT INTO projects (title, category, funding_goal, start_date, end_date) VALUE
   ("A Smarter Way to Learn Python", "books", 19.99, "07/23/2019", "07/23/2020"),
   ("Standing Desk", "office", 199.99, "09/09/2019", "09/09/2020"),
   ("Laptop", "electronics", 1499.99, "12/25/2019", "12/25/2020"),
-  ("Learn Python 3 the Hard Way", "books", 29.99, "01/01/2019", "01/01/2020"),
+  ("Mechanical Keyboard", "electronics", 299.99, "01/01/2019", "01/01/2020"),
   ("Learn Python 3 the Hard Way", "books", 29.99, "11/15/2019", "11/15/2020"),
   ("Learn Python 3 the Hard Way", "books", 29.99, "11/20/2019", "11/20/2020"),
   ("Learn Python 3 the Hard Way", "books", 29.99, "06/23/2019", "06/23/2020"),
