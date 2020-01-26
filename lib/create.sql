41d0f2d1003382d47482e0d9d7accba535c99567
ryan m <contact@ryanmeek.com>
2020-01-26 21:13:09 +0000

    Automatically backed up by Learn

--- lib/create.sql
+++ lib/create.sql
@@ -1,8 +1,8 @@
 CREATE TABLE projects (
   id INTEGER PRIMARY KEY AUTOINCREMENT,
   title TEXT,
-  category TEXT, 
-  funding_goal INTEGER, 
+  category TEXT,
+  funding_goal INTEGER,
   start_date TEXT,
   end_date TEXT
 );
